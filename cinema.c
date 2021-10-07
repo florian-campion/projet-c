12ddeaa97f384b92def5de2ad9ae041f853ae17a
florian-campion <90897804+florian-campion@users.noreply.github.com>
2021-10-07 16:40:35 +0200

    Update cinema.c

--- cinema.c
+++ cinema.c
@@ -4,61 +4,56 @@
 
 int main(void)
 {
+    // on assigne l'age à une variable nommé age
     int age = get_int("Quel est votre âge ?\n");
-    string film [3][3] = {{"Mad Max","Saw","Conjuring"},
-        {"l'arme fatale","Alien","John Wick"},
-        {"Cars","Harry potter","Narnia"}};
+    // on creer un tableau 2 dimensions avec les film de chaque categorie
+    string film [3][3] = {{"Mad Max", "Saw", "Conjuring"},
+        {"l'arme fatale", "Alien", "John Wick"},
+        {"Cars", "Harry potter", "Narnia"}
+    };
 
     int n ;
-    if(age < 11)
+    //avec un if, else if et else on va déterminer l'age pour chaque categorie et changer la valeur de n en fonction 
+    // pour qu'on arrive a la bonne categorie de film correspondant à l'age
+    if (age < 11)
     {
-         n = 2;
-    }else if( age > 11 && age <17)
+        n = 2;
+    }
+    else if (age > 11 && age < 17)
     {
-         n = 1;
-    } else
+        n = 1;
+    }
+    else
     {
         n = 0;
     }
 
-
-    for(int i = n; i < 3 ; i++)
+    // double boucle , la 1ere pour la categorie de film et la 2eme pour énuméré le choix de film et proposé si on veut le regarder
+    for (int i = n; i < 3 ; i++)
     {
-        for(int j = 0; j < 3; j++)
+        for (int j = 0; j < 3; j++)
         {
             printf("Voulez vous voir ce film : %s\n", film[i][j]);
+            //demande de confirmartion pour voir le film si oui on sort de la boucle 
             char answer = get_char("Pressez O pour oui ou N pour non \n");
-            if(answer == 'O' || answer == 'o')
+            if (answer == 'O' || answer == 'o')
             {
                 break;
             }
-            /*if(j ==2 && i== 0)
-            {
-                char answers = get_char("Voulez-vous changez de categorie ?\n Pressez O pour oui ou N pour non \n");
-                if(answers == 'O' || answers == 'o')
-                {
-                    char catg = get_char("Pressez A pour la categorie ados ou E pour la categorie enfant \n");
-                    if(catg == 'A' || catg == 'a')
-                    {
-                        i = 1;
-                    }else if (catg == 'E' || catg == 'e')
-                    {
-                        i = 2;
-                    }
-                }
-            }*/
+            
+        }  
+        //2eme verification pour le choix du film, si oui on sort de la boucle sinon on retourne dans la même categorie et on recommence
+        char answer = get_char("Etes-vous sur ?\n Pressez O pour oui ou N pour non \n");
+        if (answer == 'O' || answer == 'o')
+        {
+            printf("Bon film!\n");
+            break;
 
         }
-        char answer = get_char("Etes-vous sur ?\n Pressez O pour oui ou N pour non \n");
-            if(answer == 'O' || answer == 'o')
-            {
-                printf("Bon film!\n");
-                break;
-
-            }else
-            {
-                i--;
-            }
+        else
+        {
+            i--;
+        }
     }
 
 }
